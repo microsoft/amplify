9257e10aa8e93cb0abf6cc221e691d9aecbc7c32
Engin Polat <polatengin@hotmail.com>
2022-04-19 19:27:33 +0000

    adding copy-function functionality

--- .bash_helpers.sh
+++ .bash_helpers.sh
@@ -55,3 +55,10 @@ EOF
 
 __COMMAND_EXTENSIONS["${key}"]="${value}"
 }
+
+# create a copy of the original command
+# idea from https://stackoverflow.com/questions/1203583/how-do-i-rename-a-bash-function
+copy_function() {
+  declare -F $1 > /dev/null || return 1
+  eval "$(echo "${2}()"; declare -f ${1} | tail -n +2)"
+}
