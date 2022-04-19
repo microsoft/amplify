4e02203726b9797f2f66ffad678b832bcdd81819
Engin Polat <polatengin@hotmail.com>
2022-04-19 22:54:22 +0000

    creating  command example

--- examples/.bash_profile.sh
+++ examples/.bash_profile.sh
@@ -1,2 +1,12 @@
 # import helper functions for ebc
 source ~/.bash_helpers.sh
+
+function devops_github_help() {
+  echo "devops_github_help"
+}
+
+add_extension \
+  --command-name "devops" \
+  --sub-command-name "github" \
+  --argument-name "--help" \
+  --run "devops_github_help"
