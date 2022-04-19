985df84a93172a1104aa645deee4985ef574c325
Engin Polat <polatengin@hotmail.com>
2022-04-19 19:23:31 +0000

    declaring the core add_extension method

--- .bash_helpers.sh
+++ .bash_helpers.sh
@@ -2,3 +2,7 @@
 
 # declare global key-value pair variable
 declare -g -A __COMMAND_EXTENSIONS
+
+# to extend a command, start with a function that has the same name as the command
+function add_extension() {
+}
