47030f4716bdea5cc717cd70405886f021ef60b1
Engin Polat <polatengin@hotmail.com>
2022-04-19 22:39:04 +0000

    using gh if installed, git if it's not, to clone the ebc to the users home folder

--- install.sh
+++ install.sh
@@ -2,3 +2,12 @@
 
 # stop on error
 set -e
+
+# clone dotfiles repo
+echo $'\n########\nclone dotfiles repo\n'
+
+if type gh > /dev/null 2>&1; then
+  gh repo clone microsoft/extend-bash-commands ~/.ebc
+else
+  git clone https://github.com/microsoft/extend-bash-commands.git ~/.ebc
+fi
