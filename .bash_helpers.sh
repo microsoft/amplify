20eb537f359916b9dc66485c6482cb8f34422c01
Engin Polat <polatengin@hotmail.com>
2022-04-19 19:24:14 +0000

    parsing the arguments to the function and assign values to relevant variables

--- .bash_helpers.sh
+++ .bash_helpers.sh
@@ -5,4 +5,40 @@ declare -g -A __COMMAND_EXTENSIONS
 
 # to extend a command, start with a function that has the same name as the command
 function add_extension() {
+  POSITIONAL_ARGS=()
+
+  while [[ $# -gt 0 ]]; do
+    case ${1} in
+      -c|--command-name)
+        COMMAND_NAME="$2"
+        shift
+        shift
+        ;;
+      -s|--sub-command-name)
+        SUB_COMMAND_NAME="$2"
+        shift
+        shift
+        ;;
+      -a|--argument-name)
+        ARGUMENT_NAME="$2"
+        shift
+        shift
+        ;;
+      -r|--run)
+        RUN="$2"
+        shift
+        shift
+        ;;
+      -*|--*)
+        echo "Unknown option ${1}"
+        exit 1
+        ;;
+      *)
+        POSITIONAL_ARGS+=("${1}")
+        shift
+        ;;
+    esac
+  done
+
+  set -- "${POSITIONAL_ARGS[@]}"
 }
